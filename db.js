0021219d7445c29cfe493cf72beb8faa0988643d
Chlad <chlad@webvalley.cz>
2016-08-01 20:57:10 +0200

    optimalization save to db

--- db.js
+++ db.js
@@ -3,6 +3,8 @@ var redis = require("redis");
 var Promise = require('promise');
 var keyName = 'count';
 
+var busyUpdate = false;
+
 
 /**
  * increment count
@@ -10,25 +12,35 @@ var keyName = 'count';
  * @param Promise
  */
 function incrementCount(data) {
+    // if this function is processing it's necessary to chain self on current promise
+    if (busyUpdate !== false) {
+        return new Promise(function (fulfill, reject) {
+            return busyUpdate.then(function () {
+                incrementCount(data).then(fulfill).catch(reject);
+            });
+        });
+    }
 
-    var promise = new Promise(function (fulfill, reject) {
+    busyUpdate = new Promise(function (fulfill, reject) {
 
         var client = redis.createClient();
         client.on('connect', function () {
-            console.log('connected');
+//            console.log('connected');
             client.get(keyName, function (error, reply) {
-                console.log(1,reply, data);
+                console.log('update', reply, data);
                 var value = Number(reply) || 0;
                 value += Number(data);
                 client.set(keyName, value);
                 fulfill(value);
+                busyUpdate = false;
+
             });
         });
         client.on('error', function (error) {
             return reject(error);
         });
     });
-    return promise;
+    return busyUpdate;
 //    return promise;
 }
 /**
@@ -41,7 +53,7 @@ function getCount() {
 
         var client = redis.createClient();
         client.on('connect', function () {
-            console.log('connected');
+//            console.log('connected');
             client.get(keyName, function (error, reply) {
                 var value = Number(reply) || 0;
                 fulfill(value);

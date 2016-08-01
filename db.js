ae611719774f7f590bf0f62b2d2dfc1b0d9e8e59
Chlad <chlad@webvalley.cz>
2016-08-01 23:28:35 +0200

    db

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ var redis = require("redis");
 var Promise = require('promise');
 var keyName = 'count';
 
-var busyUpdate = false;
+var busyUpdate = null;
 
 
 /**
@@ -13,11 +13,12 @@ var busyUpdate = false;
  */
 function incrementCount(data) {
     // if this function is processing it's necessary to chain self on current promise
-    if (busyUpdate !== false) {
+    if (busyUpdate !== null) {
         return new Promise(function (fulfill, reject) {
             return busyUpdate.then(function () {
+                busyUpdate = null;
                 incrementCount(data).then(fulfill).catch(reject);
-            });
+            }).catch(reject);
         });
     }
 
@@ -25,14 +26,13 @@ function incrementCount(data) {
 
         var client = redis.createClient();
         client.on('connect', function () {
-//            console.log('connected');
             client.get(keyName, function (error, reply) {
                 console.log('update', reply, data);
                 var value = Number(reply) || 0;
                 value += Number(data);
                 client.set(keyName, value);
                 fulfill(value);
-                busyUpdate = false;
+                busyUpdate = null;
 
             });
         });
@@ -41,7 +41,6 @@ function incrementCount(data) {
         });
     });
     return busyUpdate;
-//    return promise;
 }
 /**
  * 

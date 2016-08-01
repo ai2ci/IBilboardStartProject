d925e1bf17e53a616b94c3a0f4943bfb638db2f6
Chlad <chlad@webvalley.cz>
2016-08-01 13:11:50 +0200

    the first swallow to js unit testing

--- tests/server.js
+++ tests/server.js
@@ -1,7 +1,62 @@
-describe('fileIo.addToFile', function () {
+//var http = require('http');
+var request = require('supertest');
+var chai = require('chai');
+chai.use(require("chai-as-promised"));
+var expect = chai.expect;
 
-    it('should return null', function () {
-        fileIo.addToFile().then.should.be.null;
+var server = request.agent("http://localhost:8888");
+
+describe('server response', function () {
+    it('GET /count should be 200 and not empty', function (done) {
+        server
+          .get('/count')
+          .expect(200)
+          .expect('Content-Type', /text\/plain/)
+          .end(function (err, res) {
+              if (err) {
+                  done(err);
+              }
+              expect(res.text).to.not.be.empty;
+              done();
+          });
     });
- 
+
+    it('GET /track should be 404 ', function (done) {
+        server
+          .get('/track')
+          .expect(404)
+          .expect('Content-Type', /text\/plain/)
+          .end(function (err, res) {
+              if (err) {
+                  done(err);
+              }
+              done();
+          });
+    });
+    it('POST /count should be 404 ', function (done) {
+        server
+          .post('/count')
+          .expect(404)
+          .expect('Content-Type', /text\/plain/)
+          .end(function (err, res) {
+              if (err) {
+                  done(err);
+              }
+              done();
+          });
+    });
+    it('POST /track should be 200 and empty ', function (done) {
+        server
+          .post('/track')
+          .expect(200)
+          .expect('Content-Type', /text\/plain/)
+          .end(function (err, res) {
+              if (err) {
+                  done(err);
+              }
+              expect(res.text).to.be.empty;
+              done();
+          });
+    });
+
 });
